723de0a1ed9ceaf20934cf8a36a2b9dde2eaad94
MUSIC-METRIX <108461028+Edgar-electro@users.noreply.github.com>
2023-10-02 01:27:09 +0300

    nor toshni voltmetr ard nano.ino

--- sketch_may03c.ino
+++ sketch_may03c.ino
@@ -1,4 +1,4 @@
-//input voltage via 10 kom voltage pin connect potencometr  2 .. potencometr 1 pin gnd...potencometr 3 pin connect 10 kom  dis is highi voltage input 30 volt
+//input voltage via 10 kom.arduino read pin  connect potencometr  2 .. potencometr 1 pin gnd...potencometr 3 pin connect 10 kom  dis is highi voltage input 30 volt
 //  board arduino nano 
 
  #include <ACS712.h>
@@ -6,9 +6,9 @@
  #include <LiquidCrystal_I2C.h>
 
 
-  LiquidCrystal_I2C lcd(0x23, 16, 2);
-  
-  const int  relayPin = 2;
+  LiquidCrystal_I2C lcd(0x23, 16, 2);  //(0x27, 16, 2);
+  const int  buzzerPin = 2;
+  const int  relayPin = 4;
   const int  buttonPin = 3;
   const int  voltage_pin  = A1 ;
   const int  current_pin =  A0 ;
@@ -22,7 +22,12 @@
   float watt  = 0.0;
   float VOLTtotal = 0.0;
   float AMPtotal = 0.0;
-  float voltcorect = 6.470;
+  float realVoltage = 31.92;     // Реальное напряжение, которое вы измерили (в вольтах)  stex poxem maxsimal volty testerov chapvac jisht volti masina xosqy
+  float measuredVoltage = 32.09; // Измеренное напряжение, которое Arduino показало (в вольтах)
+  float Maxviltage = 32.00;
+  float correctionFactor;        // Коэффициент коррекции
+  float correctedVoltage;        // Скорректированное напряжение (в вольтах)
+        
   float  ampcorrect = 107.0;
   float ampHours = 0.0;
 unsigned long previousMillis = 0;
@@ -36,11 +41,12 @@ unsigned long interval = 3600;
 
  
 void setup() {
+  Serial.begin(115200);
   analogReference(DEFAULT); 
   
   pinMode(voltage_pin, INPUT);
   pinMode(current_pin, INPUT);     
-  
+  pinMode(buzzerPin, OUTPUT); 
   pinMode(relayPin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
   
@@ -51,7 +57,7 @@ void setup() {
   lcd.print("Initializing...");
   delay(1000);
   lcd.clear();
-  lcd.setCursor(0, 0);
+  lcd.setCursor(1, 0);
   lcd.print("-calibration-");
   lcd.setCursor(2, 1);
   lcd.print("sensors...");
@@ -71,9 +77,11 @@ void volt_update() {
   delay(1);
   } 
   
-   voltage = voltage / 200 ;
-   VOLTtotal = voltage *(4.90 / 1023);
-   VOLTtotal = VOLTtotal * voltcorect ;
+  voltage = voltage / 200 ;
+   VOLTtotal = (voltage / 1023.0) * Maxviltage;  //stex poxem maxsimal volty testerov chapvac jisht volti masina xosqy
+   correctionFactor = realVoltage / measuredVoltage;
+   correctedVoltage = VOLTtotal * correctionFactor;
+   
 }
   void amp_update(){
   for(int i=0;i<200;i++) {
@@ -90,8 +98,8 @@ void volt_update() {
    
     void watt_update(){
     for (int i = 0; i < 10; i++) {
-    watt = AMPtotal * VOLTtotal ;
-    delay(1);
+    watt = AMPtotal * correctedVoltage ;
+    
   } 
 }
 
@@ -99,7 +107,7 @@ void AH_update(){
 unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     float currentHours = AMPtotal / 3600.0;
-    ampHours += currentHours;
+    ampHours += currentHours * 1000.0;
     previousMillis = currentMillis;
   }
 }
@@ -112,35 +120,64 @@ buttonState = digitalRead(buttonPin);
     digitalWrite(relayPin, LOW);
     relayActive = false;
   }
-delay(10);
+delay(1);
   
   prevButtonState = buttonState;
-if (AMPtotal > 5.0 && !relayActive) {
+if (AMPtotal > 6.8 && !relayActive) {
     digitalWrite(relayPin, HIGH);
     relayActive = true;
   }
 
-  delay(100);
-
+ 
  }
 
 
+void updatebuzzer() {
+
+ if (AMPtotal > 6.5 && AMPtotal < 7) {
+    blinkLED(4); // Моргаем светодиодом 4 раза
+  } else if (AMPtotal > 7) {
+    blinkLED(5); // Моргаем светодиодом 5 раз
+  } else {
+    digitalWrite(buzzerPin, LOW); // Выключаем светодиод
+  }
+
+ // delay(1000); // Делаем паузу перед следующей проверкой
+}
 
 
+void blinkLED(int times) {
+  for (int i = 0; i < times; i++) {
+    digitalWrite(buzzerPin, HIGH);
+    delay(100);
+    digitalWrite(buzzerPin, LOW);
+    delay(100);
+  }
+}
 void updateVoltage() {
   lcd.setCursor(0, 0);
   lcd.print("V");
   lcd.setCursor(1, 0);
   lcd.print(":");
-  lcd.print(VOLTtotal );
+  lcd.print(correctedVoltage);
   lcd.print(" ");
 }
 
  
-void updateCurrent() {
+void updatepower() {
   lcd.setCursor(0, 1);
-  lcd.print("I");
+  lcd.print("P");
   lcd.setCursor(1, 1);
+  lcd.print(":");
+  lcd.print(watt );     
+  lcd.print(" ");
+  
+}
+ 
+void updateCurrent() {
+  lcd.setCursor(9, 0);
+  lcd.print("I");
+  lcd.setCursor(10, 0);
   lcd.print(":");
   lcd.print(AMPtotal );
   lcd.print(" ");
@@ -148,16 +185,7 @@ void updateCurrent() {
   lcd.setCursor(0, 1); 
   lcd.print("Rely:ON"); 
   }
-}
- 
-void updatepower() {
-  lcd.setCursor(9, 0);
-  lcd.print("P");
-  lcd.setCursor(10, 0);
-  lcd.print(":");
-  lcd.print(watt );
-  lcd.print(" ");
-}
+ }
 
 void updateEnergy(){
 
@@ -172,18 +200,24 @@ void updateEnergy(){
 
 
 
-void loop() {
-        delay(500);  
+void loop() {        
+        
         volt_update();
-        amp_update();
-        watt_update();
-        AH_update();
         updateVoltage();
+        amp_update();
         updateCurrent();
+        watt_update();
         updatepower();
+        AH_update();
         updateEnergy();
         updatebutton();
-  
-  delay(0);  
+        updatebuzzer();
+    Serial.print(" Измеренное напряжение: ");
+  Serial.print(VOLTtotal);
+  Serial.print(" Скорректированное напряжение: ");
+  Serial.print(correctedVoltage);
+  Serial.print(" analog read: ");
+  Serial.print(voltRead);
+  Serial.println("%");
   
 }
